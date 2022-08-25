9afeb7b916cc8faf06ade17a5c067d4b2fdf0f85
Lukasz <lukasz.piescicki@gmail.com>
2022-08-25 15:34:30 +0200

    Add solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(first,second) {
+    let result = 0;
+    if(first >= 0 && second >= 0 && typeof first == 'number' && typeof second == 'number'){
+        if(first<second){
+            for (let i = first; i <= second; i++){
+                result += i;
+            }
+            
+        }
+        else if(first > second){
+            for(let i = second; i <= first; i++){
+                result += i;
+            }
+        }   
+    }else{
+            return 'ERROR';
+        }
+        return result;
 };
 
 // Do not edit below this line
