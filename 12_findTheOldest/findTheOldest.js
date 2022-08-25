c5d0d3edc4aad19a146e052d02837ed464ac77c4
Lukasz <lukasz.piescicki@gmail.com>
2022-08-25 20:11:45 +0200

    Add solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let peopleAge = [];
+    const currentYear = 2022;
+
+    people.forEach(person => {
+        personName = person.name;
+        if(person.yearOfDeath){
+        personAge = person.yearOfDeath - person.yearOfBirth;
+    }else{
+        personAge = currentYear - person.yearOfBirth;
+    }
+        peopleAge.push({name:personName, age:personAge});
+    });
+    peopleAge.sort((a,b) => b.age - a.age);
+    return peopleAge[0];
 
 };
 
