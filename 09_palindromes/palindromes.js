ac54c7b62ca69eb4078d01dc0bf0d073248c60a6
Lukasz <lukasz.piescicki@gmail.com>
2022-08-25 17:43:27 +0200

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    word = word.replace(regex,'').replace(/\s/g, '');
+    const splitString = word.split('');
+    const reverseArray = splitString.reverse();
+    const joinArray = reverseArray.join('');
 
+
+if(word.toLowerCase() === joinArray.toLowerCase()){
+    return true;
+}else{
+    return false;
+}
 };
 
 // Do not edit below this line
