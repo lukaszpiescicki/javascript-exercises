b4f377a311f6d73f55109f9ee12cee6c81ec40d5
Lukasz <lukasz.piescicki@gmail.com>
2022-08-25 19:28:22 +0200

    Add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(nthElement) {
+    let firstEl = 0;
+    let secondEl = 1;
+    let temp = 0;
+    
+    if(nthElement>0){
+    for (let i = 1; i<= nthElement - 1;i++){
+        temp = firstEl + secondEl;
+        firstEl = secondEl;
+        secondEl = temp;
+    }
+}else{
+    return 'OOPS';
+}
+    return secondEl;
 };
 
 // Do not edit below this line
